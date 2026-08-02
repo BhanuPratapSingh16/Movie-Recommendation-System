6dff97ac03479efbfd2ff6d7dc07772f59d733f1
Bhanu Pratap Singh <bhanuprataps1610@gmail.com>
2026-08-02 14:49:29 +0530

    Delete frontend

--- frontend/src/api/auth.js
+++
@@ -1,9 +0,0 @@
-import api from "./axios";
-
-export const register = (user) => {
-    return api.post("/auth/register", user);
-};
-
-export const login = (credentials) => {
-    return api.post("/auth/login", credentials);
-};
